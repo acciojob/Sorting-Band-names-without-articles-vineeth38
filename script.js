ce3b5b37fdec52695a289fffa4a58d75a2b5f43e
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-01 13:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-//your code here
-let bandNames = ["The Beatles", "Rolling Stones", "Led Zeppelin", "Pink Floyd", "The Who", "The Doors"];
 
-// Function to remove articles from band names
-function removeArticle(bandName) {
-  // List of articles to remove
-  let articles = ["a", "an", "the"];
-  // Split band name into words
-  let words = bandName.split(" ");
-  // Check if first word is an article
-  if (articles.includes(words[0].toLowerCase())) {
-    // Remove the article
-    words.shift();
-  }
-  // Join the remaining words and return the modified band name
-  return words.join(" ");
+const bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'Pink Floyd', 'The Rolling Stones', 'Nirvana', 'A-ha', 'The Who'];
+
+const sortedBandNames = bandNames.sort(function(a, b) {
+// Remove articles from both band names
+const regex = /^(a |an |the )/i;
+const bandA = a.replace(regex, '').trim();
+const bandB = b.replace(regex, '').trim();
+
+// Compare band names
+if (bandA < bandB) {
+return -1;
+} else if (bandA > bandB) {
+return 1;
+} else {
+return 0;
 }
-
-// Sort the band names in lexicographic order excluding articles
-bandNames.sort(function(a, b) {
-  return removeArticle(a).localeCompare(removeArticle(b));
 });
 
-// Get the ul element with id 'band'
-let ulElement = document.getElementById("band");
+// Get the unordered list element by ID
+const bandList = document.getElementById('band');
 
-// Loop through the sorted band names and add them as li elements to the ul element
-for (let i = 0; i < bandNames.length; i++) {
-  let liElement = document.createElement("li");
-  liElement.textContent = bandNames[i];
-  ulElement.appendChild(liElement);
-}
+// Add each sorted band name to the list as a list item
+sortedBandNames.forEach(function(band) {
+const li = document.createElement('li');
+li.textContent = band;
+bandList.appendChild(li);
+});
 
 
 
